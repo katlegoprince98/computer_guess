c28c47fd7053b573b5c6f8b5a21baec3a8c18445
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2022-10-26 18:14:22 +0200

    26-10-2022

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import random
 
 def guess(num):
-    ran_num = random.randint(100,num)
+    ran_num = random.randint(1,num)
     guess = 0
     while guess != ran_num:
-        guess = int(input(f"Guess a number between 100 and {num}"))
+        guess = int(input(f"Guess a number between 1 and {num}: "))
+        if guess < ran_num:
+             print("Your guess is low, please try again")
+        elif guess > ran_num:
+            print("You guess was too high please try again")
+            
+    print(f"CONGRATULATIONS!!, you guessed the number {ran_num} correctly")
+       
           
-guess(1000)
+guess(5)
